336fafff571772e79b54d2b2c9b404c27dc29694
MR ASAD <vietnamserver0341@gmail.com>
2026-07-26 02:41:52 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923415852837',
+    botName: '𝗦𝗛𝗔𝗠𝗜𝗜 𝗫 𝗠𝗥𝗔𝗦𝗔𝗗 𝗠𝗗 𝗠𝗜𝗡𝗜',
+    ownerName: '𝗦𝗛𝗔𝗠𝗜𝗜 𝗫 𝗠𝗥𝗔𝗦𝗔𝗗'
+};
